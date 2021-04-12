41d06960f20bf904a0256e0b473ecb73765f6728
Minh Tang Ba <tangbamiinh@gmail.com>
2021-04-12 07:32:28 +0700

    Authentication Demo App added

--- functions/src/https/createNote.ts
+++ functions/src/https/createNote.ts
@@ -12,22 +12,29 @@ const createNote = functions.https.onRequest(async (request, response) => {
     
     const data = request.body as Note;
 
+    // Log to the functions console the content of the request
     functions.logger.info("Request body: " + JSON.stringify(data));
 
-    sanitizeNote(data);
-
+    // Check whether the POST body exists or not
     if (!data) {
         response.status(400).send("Request Body cannot be blank");
         return;
     }
     
+    // Check whether the data of the Note exists or not
     if (!data.title || !data.content) {
         response.status(400).send("Content and title of a note are required!");
         return;
     }
 
+    // Remove all bad words from the Note
+    sanitizeNote(data);
+
     try {
+        // Add new document to the Notes collection
         const added = await firestore.collection("notes").add(data);
+
+        // Return the ID or the newly added document
         response.status(201).send(added.id);
     } catch (e) {
         functions.logger.error(e);
